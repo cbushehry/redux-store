6be962e89cf9f3e115fe6534f9066478c2b8afca
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-01-02 00:19:41 -0800

    npm run build init

---
+++ client/build/precache-manifest.01ac406fe0765474503e2156e8d43d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d116ae4d4a7ca95cfd3203664ac3e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e107f60f5d8b0adc8eb1",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "47b46580c34fcb48f158",
+    "url": "/static/js/2.6f94ce77.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.6f94ce77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e107f60f5d8b0adc8eb1",
+    "url": "/static/js/main.0e01c8a6.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
