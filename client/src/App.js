0058352cca0930b27fb4e72d5ea5920ef39be371
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-01-02 19:44:43 -0800

    removed unused declarations

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,11 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+
+import { Provider } from 'react-redux';
+import store from './utils/GlobalState';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -42,18 +44,18 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
