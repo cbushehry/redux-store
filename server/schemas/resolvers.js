a6e660abeac11d2a52f3ece79c6bb1cbcf00b9c9
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-01-02 20:07:05 -0800

    added stripe function

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
 
 const resolvers = {
   Query: {
@@ -55,23 +55,26 @@ const resolvers = {
     checkout: async (parent, args, context) => {
       const url = new URL(context.headers.referer).origin;
       const order = new Order({ products: args.products });
-      const line_items = [];
-
       const { products } = await order.populate('products').execPopulate();
 
+      const line_items = [];
+
       for (let i = 0; i < products.length; i++) {
+        // generate product id
         const product = await stripe.products.create({
           name: products[i].name,
           description: products[i].description,
           images: [`${url}/images/${products[i].image}`]
         });
 
+        // generate price id using the product id
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
           currency: 'usd',
         });
 
+        // add price id to the line items array
         line_items.push({
           price: price.id,
           quantity: 1
@@ -85,8 +88,9 @@ const resolvers = {
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`
       });
-
+      
       return { session: session.id };
+      
     }
   },
   Mutation: {
