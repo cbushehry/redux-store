8f6e8b08c8ba8598510941b0a6e8b41880b99e8a
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-01-02 20:24:30 -0800

    refactor for redux

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
+import React, { useEffect } from "react";
+import { useMutation } from "@apollo/client";
 import Jumbotron from '../components/Jumbotron';
-import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
+
+
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
@@ -13,7 +15,7 @@ function Success() {
       const products = cart.map((item) => item._id);
 
       if (products.length) {
-        const { data } = await addOrder({ variables: { products } });
+        const {data} = await addOrder({ variables: {products}});
         const productData = data.addOrder.products;
 
         productData.forEach((item) => {
@@ -29,15 +31,20 @@ function Success() {
     saveOrder();
   }, [addOrder]);
 
+
   return (
     <div>
       <Jumbotron>
         <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
+        <h2>
+          Thank you for your purchase!
+        </h2>
+        <h2>
+          You will now be redirected to the homepage
+        </h2>
       </Jumbotron>
     </div>
   );
-}
+};
 
 export default Success;
