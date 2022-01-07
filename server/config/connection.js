4e54b8fa80a2c9d0ad5add2b3f1fd295e78a6fb9
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-01-06 20:43:20 -0800

    fixed connect in config file for MongoDB, added .env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cbushehry:M@rshall716@cluster0.6scuf.mongodb.net/Cluster0?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
